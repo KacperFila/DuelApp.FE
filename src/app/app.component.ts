ab9a4e24e958f44f1edf19dfcc7e617de2b51ca0
Kacper Fila <kacperfila26@gmail.com>
2026-02-02 18:43:21 +0100

    fix: add missing environment.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from "../environments/environment.development";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DeployTest';
+  title = environment.apiUrl;
 }
