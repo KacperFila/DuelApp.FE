7af7da109246f52127045577d18e0a0f1410e920
Kacper Fila <kacperfila26@gmail.com>
2026-04-28 14:55:05 +0200

    fix: proper keycloak url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl:
     'https://staging-duelapp-be.ashyground-d2388f23.polandcentral.azurecontainerapps.io',
-  keycloakUrl: 'appkeycloak.azurewebsites.net',
+  keycloakUrl: 'https://appkeycloak.azurewebsites.net',
 };
