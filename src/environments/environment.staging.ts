4176fe8278b2d0db75ffc01ca01565c9cda2eedd
Kacper Fila <kacperfila26@gmail.com>
2026-07-30 17:47:30 +0200

    feat: update apiUrl after moving to app service

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://staging-duelapp-be.ashyground-d2388f23.polandcentral.azurecontainerapps.io',
+  apiUrl: 'https://staging-duelapp-be.azurewebsites.net',
   keycloakUrl: 'https://appkeycloak.azurewebsites.net',
   keycloakRealm: 'duelapp-realm',
   keycloakClientId: 'duelapp_fe_keycloak_client',
